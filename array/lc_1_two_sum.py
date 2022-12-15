f84ff34d09f3349cddfb38f2362cc86a505881f5
Kavya Kumar <shkasn496@gmail.com>
2022-12-15 11:35:30 -0500

    Implemented an optimized solution

--- array/lc_1_two_sum.py
+++ array/lc_1_two_sum.py
@@ -35,3 +35,24 @@ class Solution:
             if n2 in pair and pair[n2]!= i:
                 return [i, pair[n2]]
         return []
+
+"""
+Solution 3: Further optimized solution by going through O(n) one time
+Runtime 53 ms Beats 99.52%
+Memory 15.2 MB Beats 25.98%
+
+TC : O(N)
+SC : O(N)
+"""
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        if len(nums)<2:return []
+        dp={}
+        # dp = {nums[i]:i for i in range(len(nums))} #key: elem, value: index of elem
+        for i in range(len(nums)):
+            num = target-nums[i]
+            if num in dp and i!=dp[num]:
+                return [i,dp[num]]
+            dp[nums[i]]=i
+        del dp
+        return []
