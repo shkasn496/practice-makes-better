a9753ee2584ed487d910f04c57c77aa692c9f2ae
Kavya Kumar <shkasn496@gmail.com>
2022-12-27 18:54:40 -0500

    better optimized solution

--- binary_search/lc_4_median_of_2_sorted_arrays.py
+++ binary_search/lc_4_median_of_2_sorted_arrays.py
@@ -13,3 +13,35 @@ class Solution:
         merged=sorted(nums1+nums2)
         mid=0+(len(merged)-1)//2
         return merged[mid] if len(merged)%2!=0 else (merged[mid]+merged[mid+1])/2
+
+"""
+Optimized solution : Binary Search solution on smaller array & no sorting
+
+Runtime 84 ms Beats 99.11%
+Memory 14.2 MB Beats 24.12%
+
+TC: O(log(min(m,n)))
+SC: O(1)
+"""
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        if len(nums2)<len(nums1):return self.findMedianSortedArrays(nums2, nums1)
+        total=len(nums1)+len(nums2)
+        half=total//2
+        l, r=0, len(nums1)-1
+        while True:
+            partition1=l+(r-l)//2 # left partition elements in smaller array
+            partition2=half-(partition1+1)-1 # left partition elements in larger array
+
+            l1 = nums1[partition1] if partition1 >=0 else -float("inf")
+            r1 = nums1[partition1+1] if partition1+1 <len(nums1) else float("inf")
+
+            l2 = nums2[partition2] if partition2 >=0 else -float("inf")
+            r2 = nums2[partition2+1] if partition2+1 <len(nums2) else float("inf")
+            #found the correct partition
+            if l1 <=r2 and l2 <=r1:
+                if total % 2==0:return (max(l1, l2)+min(r1, r2))/2
+                return min(r1,r2)
+            elif l1 > r2:r=partition1-1 # move left boundary partition one step to the left
+            elif l2 > r1:l=partition1+1 # move left boundary partition one step to the right
+        return -1
