247c9ae1f716badf4e815055f54064ad5f07cd86
Kavya Kumar <shkasn496@gmail.com>
2023-01-03 17:53:31 -0500

    union find solution

--- dfs_bfs/lc_200_number_of_islands.py
+++ dfs_bfs/lc_200_number_of_islands.py
@@ -53,3 +53,51 @@ class Solution:
                     if dfs(r,c)>0: island_count+=1
         del directions
         return island_count
+
+"""
+Solution 3: Union Find
+
+Runtime 321 ms Beats 82.5%
+Memory 20.3 MB Beats 37.66%
+
+TC: O(m*n)
+SC:O(m*n)
+"""
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        m, n = len(grid), len(grid[0])
+        directions = [(0,1), (1,0)]
+        uf = UnionFind(m*n)
+        island_count=0
+        for r in range(m):
+            for c in range(n):
+                if grid[r][c] =='0': continue
+                island_count+=1
+                index_elem = r*n+c
+                for dr, dc in directions:
+                    nr, nc = r+dr, c+dc
+                    if 0<=nr<m and 0<=nc<n and grid[nr][nc]=='1':
+                        index_ngbr = nr*n+nc
+                        island_count-= uf.union(index_elem, index_ngbr)
+        del uf, directions
+        return island_count
+
+class UnionFind():
+    def __init__(self, size):
+        self.groups = [i for i in range(size)]
+        self.rank = [0]*size
+        return
+    def find(self, x):
+        if self.groups[x]==x:return x
+        return self.find(self.groups[x])
+    def union(self, x, y):
+        parent_x = self.find(x)
+        parent_y = self.find(y)
+        if parent_x==parent_y:return 0
+        if self.rank[parent_x]>=self.rank[parent_y]:
+            self.groups[parent_y]=parent_x
+            self.rank[parent_x]+=1
+        else :
+            self.groups[parent_x]=parent_y
+            self.rank[parent_y]+=1
+        return 1
