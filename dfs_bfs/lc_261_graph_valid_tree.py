0ee8d52f91766b3e8abf25be3a2b812c889db0e7
Kavya Kumar <shkasn496@gmail.com>
2023-01-03 19:10:21 -0500

    solution with union find

--- dfs_bfs/lc_261_graph_valid_tree.py
+++ dfs_bfs/lc_261_graph_valid_tree.py
@@ -41,3 +41,44 @@ class Solution:
             adj_list[edge[0]].append(edge[1])
             adj_list[edge[1]].append(edge[0])
         return adj_list
+
+"""
+Solution 2: Union Find
+
+Runtime 91 ms Beats 96.15%
+Memory 15.3 MB Beats 91.30%
+
+TC: O(e+n) where e=len(edges), n =no of nodes
+SC: O(n)
+"""
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        if n==1:return True
+        if n-1 !=len(edges):return False
+        uf = UnionFind(n)
+        for (n1,n2) in edges:
+            # if two nodes have same parent before calling union, there is a cycle
+            if uf.find(n1)==uf.find(n2):return False #check for cycle
+            uf.union(n1, n2)
+            n-=1
+        del uf
+        return n==1
+
+class UnionFind():
+    def __init__(self, size):
+        self.parent=[i for i in range(size)]
+        self.rank = [0]*size
+        return
+    def find(self, x):
+        if self.parent[x]==x:return x
+        return self.find(self.parent[x])
+    def union(self, x, y):
+        parent_x, parent_y = self.find(x), self.find(y)
+        if parent_x==parent_y:return False
+        if self.rank[parent_x]>=self.rank[parent_y]:
+            self.parent[parent_y]=parent_x
+            self.rank[parent_x]+=1
+        else:
+            self.parent[parent_x]=parent_y
+            self.rank[parent_y]+=1
+        return True
