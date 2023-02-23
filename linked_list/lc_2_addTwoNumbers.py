316326dd4d406573e944e409585130316cb545de
Kavya Kumar <shkasn496@gmail.com>
2023-02-23 16:23:02 -0500

    updated solution with comments

--- linked_list/lc_2_addTwoNumbers.py
+++ linked_list/lc_2_addTwoNumbers.py
@@ -12,20 +12,21 @@ TC = SC = O(n)
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        l3=ListNode()
+        if not l1 or not l2:return l1 if l1 else l2
+        l3 = ListNode()
         head = l3
         carry=0
         while l1 or l2 or carry:
-            num3=carry
+            val = carry
             if l1:
-                num3+=l1.val
+                val+=l1.val
                 l1=l1.next
             if l2:
-                num3+=l2.val
+                val+=l2.val
                 l2=l2.next
-            carry = num3 // 10
-            l3.val = num3 % 10
-            if l1 or l2 or carry: 
+            carry = val // 10 #quotient
+            l3.val = val % 10 # remainder
+            if l1 or l2 or carry:
                 l3.next=ListNode()
                 l3=l3.next
         return head
