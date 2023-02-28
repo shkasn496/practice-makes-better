297c6819ec194b18775feaecfdc8575dd29116c5
Kavya Kumar <shkasn496@gmail.com>
2023-02-28 14:28:44 -0500

    faster solution

--- backtracking/lc_40_combination_sum_ii.py
+++ backtracking/lc_40_combination_sum_ii.py
@@ -1,28 +1,32 @@
 # https://leetcode.com/problems/combination-sum-ii/description/
 """
 Solution : Backtracking
-Runtime 70 ms Beats 77.88% 
-Memory 14.1 MB Beats 18.37%
+Runtime 65 ms Beats 77.9%
+Memory 14.1 MB Beats 17.32%
 TC: O(2^N) because every step there is a choice to add or not add the number
 SC: O(N)
 """
 class Solution:
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        if len(candidates)==1 and target>candidates[0]:return []
+        N = len(candidates)
         candidates.sort()
-        if target < candidates[0]:return []
-        result=set()
-        def backtrack(idx, sum_values, sub_array):
-            # conditions
-            if sum_values > target:return
-            # goal
-            if sum_values == target:
-                if tuple(sub_array) not in result:
-                    result.add(tuple(sub_array))
+        combinations = set()
+        def backtrack(idx, sum_arr, subarray):
+            # 1. Conditions
+            if idx > N or sum_arr>target:return
+            # 2. Goal
+            if sum_arr==target:
+                comb=tuple(subarray)
+                if comb not in combinations:
+                    combinations.add(comb)
+                del comb
                 return
-            # choices
-            for i in range(idx,len(candidates)):
-                if i > idx and candidates[i]==candidates[i-1]:
-                    continue
-                backtrack(i+1,sum_values+candidates[i], sub_array+[candidates[i]])
-        backtrack(0,0,[])
-        return result
+            # 3. Choices
+            for i in range(idx, N):
+                if i>idx and candidates[i]==candidates[i-1]:continue
+                if candidates[i] > target:break
+                backtrack(i+1, sum_arr+candidates[i], subarray+[candidates[i]])
+            return
+        backtrack(0, 0, [])
+        return combinations
