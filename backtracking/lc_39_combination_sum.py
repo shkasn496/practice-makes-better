3f5b610d94a8e199ecafed945dfa0a67f8617576
Kavya Kumar <shkasn496@gmail.com>
2023-02-28 14:19:26 -0500

    solution update

--- backtracking/lc_39_combination_sum.py
+++ backtracking/lc_39_combination_sum.py
@@ -1,9 +1,8 @@
 # https://leetcode.com/problems/combination-sum/description/
 """
 Solution 1: Backtracking
-Runtime 47 ms Beats 97.12%
-Memory 14.1 MB Beats 9.25%
-
+Runtime 46 ms Beats 97.34%
+Memory 14.3 MB Beats 8.54%
 TC: O(N^T/M) where T is target, M is min value, N is list length
     every step there is a choice to add the number 0-N times 
     which is why it is N raised to T instead of 2 ^ T.
@@ -12,21 +11,24 @@ SC: O(T/M) in the last level of recursion
 """
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        if len(candidates)==1 and target < candidates[0]:return []
+        if len(candidates)==1 and target>candidates[0]:return []
+        N = len(candidates)
         candidates.sort()
-        result=set()
-        def backtrack(idx, sub_array):
-            sum_values=sum(sub_array)
-            # conditions
-            if sum_values>target:return
-            # goal
-            if sum_values==target:
-                if tuple(sub_array) not in result:
-                    result.add(tuple(sub_array))
+        combinations = set()
+        def backtrack(idx, sum_arr, subarray):
+            # 1. Conditions
+            if idx > N or sum_arr>target:return
+            # 2. Goal
+            if sum_arr==target:
+                comb=tuple(subarray)
+                if comb not in combinations:
+                    combinations.add(comb)
+                del comb
                 return
-            # choices
-            for i in range(idx,len(candidates)):
-                if sum_values+candidates[i]<=target:
-                    backtrack(i,sub_array+[candidates[i]])
-        backtrack(0,[])
-        return list(result)
+            # 3. Choices
+            for i in range(idx, N):
+                if sum_arr+candidates[i]<=target:
+                    backtrack(i, sum_arr+candidates[i], subarray+[candidates[i]])
+            return
+        backtrack(0, 0, [])
+        return combinations
