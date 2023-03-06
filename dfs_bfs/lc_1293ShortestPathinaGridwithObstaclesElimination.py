209193ee6749144ddd2a69e0311ad2e3548592a9
Kavya Kumar <shkasn496@gmail.com>
2023-03-06 12:53:30 -0500

    cleaner solution

--- dfs_bfs/lc_1293ShortestPathinaGridwithObstaclesElimination.py
+++ dfs_bfs/lc_1293ShortestPathinaGridwithObstaclesElimination.py
@@ -9,21 +9,22 @@ SC: O(m*n*k)
 from queue import Queue
 class Solution:
     def shortestPath(self, grid: List[List[int]], k: int) -> int:
-        m, n = len(grid), len(grid[0])
-        if k >= m+n-2:return m+n-2
-        directions = [(0,1),(0,-1),(1,0),(-1,0)]
-        queue = Queue()
-        queue.put((0,0,k,0))#r, c, k, steps
-        visited = set()
+        m,n=len(grid), len(grid[0])
+        # check if we can eliminate more obstacles that shortestPath
+        if k>=m+n-2:return m+n-2
+        directions=[(0,1),(0,-1),(1,0),(-1,0)]
+        visited=set()#row,col, k
+        queue =Queue()
+        queue.put((0,0,k,0))#row, col, k , step
         while not queue.empty():
-            r, c, k, step = queue.get()
-            if r==m-1 and c == n-1:return step
+            r, c, k, step=queue.get()
+            if (r,c) == (m-1,n-1):return step
+            if (r,c,k) in visited:continue
+            visited.add((r,c,k))
             for dr, dc in directions:
                 nr, nc = r+dr, c+dc
-                if (0<=nr<m) and (0<=nc<n):
-                    nk= k - grid[nr][nc]
-                    if nk>=0 and (nr, nc, nk ) not in visited:
-                        visited.add((nr, nc, nk))
-                        queue.put((nr, nc, nk,step+1))
-        del visited, queue, directions
+                if not 0<=nr<m or not 0<=nc<n or (k<=0 and grid[nr][nc]==1):continue
+                if k>0 and grid[nr][nc]==1:queue.put((nr,nc,k-1,step+1))
+                elif grid[nr][nc]==0:queue.put((nr,nc,k,step+1))
+        del queue, visited, directions
         return -1
