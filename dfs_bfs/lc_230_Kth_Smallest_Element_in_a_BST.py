477b0dd7482ff67a877fba6fef2cd2951272384f
Kavya Kumar <shkasn496@gmail.com>
2023-05-27 20:48:58 -0400

    solution

--- dfs_bfs/lc_230_Kth_Smallest_Element_in_a_BST.py
+++ dfs_bfs/lc_230_Kth_Smallest_Element_in_a_BST.py
@@ -6,7 +6,7 @@ Details
 Runtime: 55 ms, faster than 92.64% of Python3 online submissions for Kth Smallest Element in a BST.
 Memory Usage: 18 MB, less than 89.08% of Python3 online submissions for Kth Smallest Element in a BST.
 
-TC: O(n) worst case O(logN) average case for balanced tree height=logN
+TC: O(n) to build traversal
 SC:  O(n)
 """
 # Definition for a binary tree node.
@@ -21,3 +21,25 @@ class Solution:
             return inorder_traversal(node.left)+[node.val]\
         +inorder_traversal(node.right) if node else []
         return inorder_traversal(root)[k-1]
+
+"""
+Solution 2: Better optimized inorder traversal. Only traversing height of tree.
+Runtime 55 ms Beats 72.97%
+Memory 20.5 MB Beats 18.96%
+TC:O(logN+k) Balanced Tree
+    O(N + k) skewed tree with all the nodes in the left subtree.
+SC: O(k) + O(H) output array + recursive stack for height of tree
+"""
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        stack=[]
+        def inorder_traversal(node):
+            if not node:return
+            if node.left:inorder_traversal(node.left)
+            if len(stack)<k:stack.append(node.val)
+            if len(stack)<k:inorder_traversal(node.right)
+            return
+        inorder_traversal(root)
+        k_smallest=stack[-1]
+        del stack
+        return k_smallest
