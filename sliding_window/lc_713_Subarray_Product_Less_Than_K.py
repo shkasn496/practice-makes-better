c78c176f383a773ba3c26aefac8e0ab70f960b62
Kavya Kumar <shkasn496@gmail.com>
2023-07-24 13:55:58 -0400

    minor adjustments to categories

--- sliding_window/lc_713_Subarray_Product_Less_Than_K.py
+++ sliding_window/lc_713_Subarray_Product_Less_Than_K.py
@@ -20,5 +20,21 @@ class Solution:
         return sub_array_count
 
 """
-Solution 2: Two pointers + Sliding Window
+Solution 2: Sliding Window
+Runtime 651 ms Beats 99.9%
+Memory 19.3 MB Beats 96.63%
+TC:O(n)
+SC:O(1)
 """
+class Solution:
+    def numSubarrayProductLessThanK(self, nums: List[int], k: int) -> int:
+        if k <= 1:return 0
+        ans = left = 0
+        product = 1
+        for right in range(len(nums)):
+            product *= nums[right]
+            while product >= k:
+                product//= nums[left]
+                left += 1
+            ans += right-left+1
+        return ans
