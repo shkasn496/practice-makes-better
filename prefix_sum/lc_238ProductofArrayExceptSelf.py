f799daa9c8f9f537cc991a631a8e199209c26ea0
Kavya Kumar <shkasn496@gmail.com>
2023-08-18 17:57:59 -0400

    solution

--- prefix_sum/lc_238ProductofArrayExceptSelf.py
+++ prefix_sum/lc_238ProductofArrayExceptSelf.py
@@ -1,21 +1,25 @@
 # https://leetcode.com/problems/product-of-array-except-self/description/
 """
-Solution : Optimized solution
-Runtime 222 ms Beats 98.21%
-Memory 21.2 MB Beats 75.70%
+Solution : Calculate prefix product  of all elements first.
+            Instead of storing another array to store suffix products and then multiply the two arrays,
+            I simply store the current suffix in a variable and update it with the nums array.
+
+Runtime 175 ms Beats 99.55%
+Memory 24.1 MB Beats 57.4%
+
 TC: O(n)
 SC: O(1)
 """
 
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        n, postfix = len(nums), 1
-        products = [1]*n
+        n, suffix = len(nums), 1
+        prefix = [1]*n
         # calculate prefix products of elements
         for i in range(1, n):
-            products[i]=products[i-1]*nums[i-1]
-        #calculate postfix products of elements
+            prefix[i]=prefix[i-1]*nums[i-1]
+        #calculate suffix products of elements
         for i in range(len(nums)-1, -1, -1):
-            products[i]*=postfix
-            postfix*=nums[i]
-        return products
+            prefix[i]*=suffix
+            suffix*=nums[i]
+        return prefix
