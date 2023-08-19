bfb71c3badede456ab12c67eb101df7b2129bab1
Kavya Kumar <shkasn496@gmail.com>
2023-08-19 16:16:50 -0400

    solution update

--- sliding_window/lc_713_Subarray_Product_Less_Than_K.py
+++ sliding_window/lc_713_Subarray_Product_Less_Than_K.py
@@ -2,25 +2,34 @@
 
 """
 Solution 1: Brute Force. Works but give Time Limit Exceeded.
-TC: O(n!)
+TC: O(n^2)
 SC:O(1)
 """
 class Solution:
     def numSubarrayProductLessThanK(self, nums: List[int], k: int) -> int:
-        if k==0:return k
-        if len(nums)==1:return nums[0] if nums[0]<k else 0
-        sub_array_count=0
+        result = 0
+        if k<=1:return result
         for i in range(len(nums)):
             if nums[i]>=k:continue
-            prod=1
-            for n in nums[i:len(nums)]:
-                prod *= n
-                if prod<k: sub_array_count+=1
-                else: break
-        return sub_array_count
+            result+=1
+            product=nums[i]
+            for j in range(i+1, len(nums)):
+                if product*nums[j]>=k:break
+                product*=nums[j]
+                result+=1
+        return result
 
 """
 Solution 2: Sliding Window
+            ans += right - left + 1 in "Sliding Window" approach. 
+            Because the element pointed by the "right" is the new one 
+            we are investigating, as long as we find a subarray with 
+            prod < k after being divided by nums[left], ans should be 
+            increased by the length of the subarray because the product 
+            is ever increasing. For example, [10,5,2,6], when right 
+            points to 6, left points to 5, ans added 3, which is due 
+            to [5,2,6],[2.6],and[6].
+
 Runtime 651 ms Beats 99.9%
 Memory 19.3 MB Beats 96.63%
 TC:O(n)
@@ -28,13 +37,13 @@ SC:O(1)
 """
 class Solution:
     def numSubarrayProductLessThanK(self, nums: List[int], k: int) -> int:
-        if k <= 1:return 0
-        ans = left = 0
-        product = 1
-        for right in range(len(nums)):
-            product *= nums[right]
+        result = 0
+        if k<=1:return result
+        left, product = 0, 1
+        for right, val in enumerate(nums):
+            product*=val
             while product >= k:
-                product//= nums[left]
-                left += 1
-            ans += right-left+1
-        return ans
+                product /= nums[left] # divide left pointer element
+                left+=1
+            result += right - left + 1 #sliding window
+        return result
