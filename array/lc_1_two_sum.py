3355f339e902ee5dc3dfec32d7dc265aa663d930
shkasn496 <shkasn496@gmail.com>
2024-12-13 19:58:43 -0800

    updated solution

--- array/lc_1_two_sum.py
+++ array/lc_1_two_sum.py
@@ -9,19 +9,17 @@ SC: O(1)
 """
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        if len(nums) == 2: return [0,1]
         for i in range(len(nums)):
-            n2 = target - nums[i]
-            if n2 in nums:
-                try:
-                    j = nums.index(n2)
-                    if i!=j:return [i,j]
-                except:continue
-        return []
+            for j in range(i+1, len(nums)):
+                nums1, nums2 = nums[i], nums[j]
+                if nums1 + nums2 == target:
+                    return [i,j]
 
 """
-Solution 2: Optimized solution
-Runtime 58 ms Beats 97.81%
-Memory 15.1 MB Beats 69.97%
+Solution 2: Optimized solution, going through array only once
+Runtime 0 ms Beats 100%
+Memory 18.32 MB Beats 14.94%
 
 TC : O(N)
 SC : O(N)
@@ -29,30 +27,14 @@ SC : O(N)
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        pair = {nums[i]: i for i in range(len(nums))}
-        for i in range(len(nums)):
-            n2 =target-nums[i]
-            if n2 in pair and pair[n2]!= i:
-                return [i, pair[n2]]
-        return []
-
-"""
-Solution 3: Further optimized solution by going through O(n) one time
-Runtime 53 ms Beats 99.52%
-Memory 15.2 MB Beats 25.98%
-
-TC : O(N)
-SC : O(N)
-"""
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        if len(nums)<2:return []
-        dp={}
-        # dp = {nums[i]:i for i in range(len(nums))} #key: elem, value: index of elem
-        for i in range(len(nums)):
-            num = target-nums[i]
-            if num in dp and i!=dp[num]:
-                return [i,dp[num]]
-            dp[nums[i]]=i
-        del dp
-        return []
+        if len(nums) == 2: return [0,1]
+        mapping = {}
+        answer = []
+        for i, num1 in enumerate(nums):
+            num2 = target - num1
+            if num2 in mapping:
+                answer = [i, mapping[num2]]
+                break
+            mapping[num1] = i
+        del mapping
+        return answer
