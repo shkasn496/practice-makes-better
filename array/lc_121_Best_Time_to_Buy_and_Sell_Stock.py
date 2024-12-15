c624631a2e288c8deff5bf6fb93bbc1d8bec076c
shkasn496 <shkasn496@gmail.com>
2024-12-14 18:19:49 -0800

    updated solution

--- array/lc_121_Best_Time_to_Buy_and_Sell_Stock.py
+++ array/lc_121_Best_Time_to_Buy_and_Sell_Stock.py
@@ -8,10 +8,11 @@ SC:O(1)
 """
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        min_price = float('inf')
-        max_profit = 0
-        for price in prices:
-            if price < min_price: min_price = price
-            elif price - min_price > max_profit:
-                max_profit = price - min_price
-        return max_profit
+        if len(prices) < 2:return 0
+        buy_price = prices[0]
+        profit = 0
+        for sell_price in prices[1:]:
+            if sell_price < buy_price:
+                buy_price = sell_price
+            else:profit = max(profit, sell_price-buy_price)
+        return profit
