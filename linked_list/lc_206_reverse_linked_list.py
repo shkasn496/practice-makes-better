a1e3f9ffe4399e4624de85c7b2a5168fd049fa55
shkasn496 <shkasn496@gmail.com>
2025-01-05 19:17:58 -0800

    updated two solutions

--- linked_list/lc_206_reverse_linked_list.py
+++ linked_list/lc_206_reverse_linked_list.py
@@ -1,10 +1,10 @@
 # https://leetcode.com/problems/reverse-linked-list/description/
 """
 Solution 1: Iterative
-Runtime 31 ms Beats 93.37%
-Memory 15.4 MB Beats 90.42%
+Runtime 0 ms Beats 100%
+Memory 18.60MB Beats 25.74%
 TC: O(n)
-SC:O(1)
+SC: O(1)
 """
 # Definition for singly-linked list.
 # class ListNode:
@@ -13,25 +13,35 @@ SC:O(1)
 #         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        if not head:return head
+        if not head: return head
         prev = None
         while head:
-            nxt = head.next
+            next_node = head.next
             head.next = prev
             prev = head
-            head = nxt
+            head = next_node
         return prev
 
 """
 Solution 2: Recursive
-Runtime 29 ms Beats 96.19%
-Memory 20.5 MB Beats 6.47%
-TC=SC=O(n)
+Runtime 0 ms Beats 100%
+Memory 19.02MB Beats 12.24%
+
+TC: O(n)
+SC: O(n)
 """
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
+    def recursive_helper(self, prev, head):
+        if not head: return prev
+        next_node = head.next
+        head.next = prev
+        return self.recursive_helper(head, next_node)
+
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        if not head or not head.next:return head
-        prev = self.reverseList(head.next)
-        head.next.next = head
-        head.next = None
-        return prev
+        if not head: return head
+        return self.recursive_helper(None, head)
