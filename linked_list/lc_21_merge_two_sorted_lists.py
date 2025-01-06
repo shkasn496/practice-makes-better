2aa2ae6d13ede9afd198be734bd5187d1a6904c7
shkasn496 <shkasn496@gmail.com>
2025-01-05 18:18:22 -0800

    updated two solutions

--- linked_list/lc_21_merge_two_sorted_lists.py
+++ linked_list/lc_21_merge_two_sorted_lists.py
@@ -1,11 +1,11 @@
 # https://leetcode.com/problems/merge-two-sorted-lists/description/
 """
 Solution 1: Iterative solution
-Runtime 31 ms Beats 95.58%
-Memory 13.8 MB Beats 98.59%
+Runtime 0 ms Beats 100%
+Memory 17.7 MB Beats 27.93%
 
 TC: O(m+n)
-SC:O(m+n)
+SC: O(1)
 """
 # Definition for singly-linked list.
 # class ListNode:
@@ -14,15 +14,36 @@ SC:O(m+n)
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        if not list1 or not list2:return list1 if list1 else list2
-        prev = prehead = ListNode()
+        if not list1 or not list2:return list1 or list2
+        head = prev = ListNode()
         while list1 and list2:
             if list1.val <= list2.val:
                 prev.next = list1
-                list1=list1.next
+                list1 = list1.next
             else:
-                prev.next=list2
-                list2=list2.next
-            prev=prev.next
-        prev.next=list1 or list2 #add leftover list
-        return prehead.next
+                prev.next = list2
+                list2 = list2.next
+            prev = prev.next
+        prev.next = list1 or list2 #add leftover list
+        return head.next
+
+"""
+Solution 2: Recursive solution
+
+TC: O(m+n)
+SC: O(m+n) Call stack space
+"""
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        if not list1 or not list2: return list1 or list2
+        if list1.val <= list2.val:
+            list1.next = self.mergeTwoLists(list1.next, list2)
+            return list1
+        else:
+            list2.next = self.mergeTwoLists(list1, list2.next)
+            return list2
